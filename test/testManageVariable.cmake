e7ea8efd58c1bc8e4602a161870f1af54604a7c1
Ding-Yi Chen <dingyichen@gmail.com>
2010-09-28 00:41:19 +1000

    Finish test cases

--- test/testManageVariable.cmake
+++ test/testManageVariable.cmake
@@ -3,6 +3,8 @@ INCLUDE(test/testCommon.cmake)
 INCLUDE(ManageVariable)
 SETTING_FILE_GET_VARIABLE(var_q_1 "VAR_Q_1" test/sample-setting.txt )
 TEST_STR_MATCH(var_q_1 "Kudo")
+SETTING_FILE_GET_VARIABLE(var_q_4 "VAR_Q_4" test/sample-setting.txt )
+TEST_STR_MATCH(var_q_4 "Kudo\;Good")
 
 SETTING_FILE_GET_ALL_VARIABLES(test/sample-setting.txt)
 TEST_STR_MATCH(VAR_Q_1 "Kudo")
