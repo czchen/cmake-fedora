47eb50af6630d093570c24a7c0eb6fa7f2badfbd
Ding-Yi Chen <dingyichen@gmail.com>
2010-12-10 00:54:05 +1000

    Add back-slash test cases

--- test/testManageVariable.cmake
+++ test/testManageVariable.cmake
@@ -11,11 +11,19 @@ SETTING_FILE_GET_VARIABLE(var_q_4_noescape_sc "VAR_Q_4" test/sample-setting.txt
 TEST_STR_MATCH(var_q_4_noescape_sc "Kudo;Good")
 #MESSAGE("var_q_4_noescape_sc=|${var_q_4_noescape_sc}|")
 
+SETTING_FILE_GET_VALUE(var_slash_1 "VAR_SLASH_1" test/sample-setting.txt)
+TEST_STR_MATCH(var_slash_1 "With 2 SLASHES")
+
 SETTING_FILE_GET_ALL_VARIABLES(test/sample-setting.txt)
 TEST_STR_MATCH(VAR_Q_1 "Kudo")
 TEST_STR_MATCH(VAR_Q_2 "Kudo")
 TEST_STR_MATCH(VAR_Q_3 "Kudo ")
 TEST_STR_MATCH(VAR_Q_4 "Kudo\;Good")
+
+# Test whether back-slash is well handled
+TEST_STR_MATCH(VAR_SLASH_2 "TestSLASH")
+TEST_STR_MATCH(VAR_SLASH_3 "Will \n and \nOK?")
+
 
 # Test whether ${var} is successfully substitute
 SETTING_FILE_GET_ALL_VARIABLES(test/sample-setting2.txt)
