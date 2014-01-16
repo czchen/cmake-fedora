59527054d65f73df97b79eb5cd32ad7250025f89
Ding-Yi Chen <dingyichen@gmail.com>
2014-01-16 14:40:22 +1000

    ManageTarget: ADD_CUSTOM_TARGET_COMMAND: replace MAKE to NO_FORCE

--- Modules/ManageTarget.cmake
+++ Modules/ManageTarget.cmake
@@ -5,7 +5,7 @@
 #
 # Defines following functions:
 #   ADD_CUSTOM_TARGET_COMMAND(target OUTPUT file1 [file2 ..]
-#     [ALL] [MAKE] COMMAND command1 ...
+#     [ALL] [NO_FORCE] COMMAND command1 ...
 #   )
 #   - Combine ADD_CUSTOM_TARGET and ADD_CUSTOM_COMMAND.
 #     This command is handy if you want a target that always refresh
@@ -13,14 +13,14 @@
 #     in separate ADD_CUSTOM_TARGET and ADD_CUSTOM_COMMAND.
 #
 #     If you also want a target that run only if output files 
-#     do not exist or outdated. Specify "MAKE".
+#     do not exist or outdated. Specify "NO_FORCE".
 #     The target for that will be "<target>/make".
 #
 #     * Parameters:
 #       + target: target for this command
 #       + OUTPUT file1, file2 ... : Files to be outputted by this command
 #       + ALL: (Optional) The target is built with target 'all'
-#       + MAKE: (Optional) Produce a target that run only if output files
+#       + NO_FORCE: (Optional) Produce a target that run only if output files
 #         do not exist or outdated. 
 #       + command1 ... : Command to be run. 
 #         The rest arguments are same with  ADD_CUSTOM_TARGET.
@@ -30,7 +30,7 @@ IF(NOT DEFINED _MANAGE_TARGET_CMAKE_)
     SET(_MANAGE_TARGET_CMAKE_ "DEFINED")
     INCLUDE(ManageVariable)
     FUNCTION(ADD_CUSTOM_TARGET_COMMAND target)
-	SET(_validOptions "OUTPUT" "ALL" "MAKE" "COMMAND")
+	SET(_validOptions "OUTPUT" "ALL" "NO_FORCE" "COMMAND")
 	VARIABLE_PARSE_ARGN(_opt _validOptions ${ARGN})
 	IF(DEFINED _opt_ALL)
 	    SET(_all "ALL")
@@ -46,11 +46,11 @@ IF(NOT DEFINED _MANAGE_TARGET_CMAKE_)
 	    COMMAND ${_opt_COMMAND}
 	    )
 
-	IF(DEFINED _opt_MAKE)
-	    ADD_CUSTOM_TARGET(${target}/make
+	IF(DEFINED _opt_NO_FORCE)
+	    ADD_CUSTOM_TARGET(${target}/no_force
 		DEPENDS ${_opt_OUTPUT}
 		)
-	ENDIF(DEFINED _opt_MAKE)
+	ENDIF(DEFINED _opt_NO_FORCE)
     ENDFUNCTION(ADD_CUSTOM_TARGET_COMMAND)
 
 ENDIF(NOT DEFINED _MANAGE_TARGET_CMAKE_)
