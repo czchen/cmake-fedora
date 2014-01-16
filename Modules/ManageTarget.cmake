77313d38b1e40519a7f7d5b55356821d4b2325d2
Ding-Yi Chen <dingyichen@gmail.com>
2014-01-16 12:06:59 +1000

    ManageTranslation: ADD_CUSTOM_TARGET_COMMAND: MAKE: new option

--- Modules/ManageTarget.cmake
+++ Modules/ManageTarget.cmake
@@ -3,24 +3,34 @@
 # Includes:
 #   ManageVariable
 #
-# Defines following macros:
+# Defines following functions:
 #   ADD_CUSTOM_TARGET_COMMAND(target OUTPUT file1 [file2 ..]
-#   [ALL] COMMAND
-#   command1 ...)
+#     [ALL] [MAKE] COMMAND command1 ...
+#   )
 #   - Combine ADD_CUSTOM_TARGET and ADD_CUSTOM_COMMAND.
-#     Always build when making the target, also specify the output files
-#     Arguments:
-#     + target: target for this command
-#     + file1, file2 ... : Files to be outputted by this command
-#     + command1 ... : Command to be run. The rest arguments are same with
-#                      ADD_CUSTOM_TARGET.
+#     This command is handy if you want a target that always refresh
+#     the output files without writing the same build recipes
+#     in separate ADD_CUSTOM_TARGET and ADD_CUSTOM_COMMAND.
+#
+#     If you also want a target that run only if output files 
+#     do not exist or outdated. Specify "MAKE".
+#     The target for that will be "<target>/make".
+#
+#     * Parameters:
+#       + target: target for this command
+#       + OUTPUT file1, file2 ... : Files to be outputted by this command
+#       + ALL: (Optional) The target is built with target 'all'
+#       + MAKE: (Optional) Produce a target that run only if output files
+#         do not exist or outdated. 
+#       + command1 ... : Command to be run. 
+#         The rest arguments are same with  ADD_CUSTOM_TARGET.
 #
 
 IF(NOT DEFINED _MANAGE_TARGET_CMAKE_)
     SET(_MANAGE_TARGET_CMAKE_ "DEFINED")
     INCLUDE(ManageVariable)
-    MACRO(ADD_CUSTOM_TARGET_COMMAND target)
-	SET(_validOptions "OUTPUT" "ALL" "COMMAND")
+    FUNCTION(ADD_CUSTOM_TARGET_COMMAND target)
+	SET(_validOptions "OUTPUT" "ALL" "MAKE" "COMMAND")
 	VARIABLE_PARSE_ARGN(_opt _validOptions ${ARGN})
 	IF(DEFINED _opt_ALL)
 	    SET(_all "ALL")
@@ -35,7 +45,13 @@ IF(NOT DEFINED _MANAGE_TARGET_CMAKE_)
 	ADD_CUSTOM_COMMAND(OUTPUT ${_opt_OUTPUT} 
 	    COMMAND ${_opt_COMMAND}
 	    )
-    ENDMACRO(ADD_CUSTOM_TARGET_COMMAND)
+
+	IF(DEFINED _opt_MAKE)
+	    ADD_CUSTOM_TARGET(${target}/make
+		DEPENDS ${_opt_OUTPUT}
+		)
+	ENDIF(DEFINED _opt_MAKE)
+    ENDFUNCTION(ADD_CUSTOM_TARGET_COMMAND)
 
 ENDIF(NOT DEFINED _MANAGE_TARGET_CMAKE_)
 
