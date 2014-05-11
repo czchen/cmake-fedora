c1bfa3eb3bf025c30dcccc8d4312673e9afbe547
Ding-Yi Chen <dingyichen@gmail.com>
2014-05-12 00:24:42 +1000

    Fixed Bug 1093336 - date(1): -u is much portable than --utc

--- Modules/DateTimeFormat.cmake
+++ Modules/DateTimeFormat.cmake
@@ -28,7 +28,7 @@ IF(NOT DEFINED _DATE_TIME_FORMAT_CMAKE_)
 	IF(_locale)
 	    SET(ENV{LC_ALL} ${_locale})
 	ENDIF(_locale)
-	COMMAND_OUTPUT_TO_VARIABLE(${date_var} date --utc "${format}")
+	COMMAND_OUTPUT_TO_VARIABLE(${date_var} date -u "${format}")
     ENDMACRO(TODAY date_var format)
 
     TODAY(TODAY_CHANGELOG "+%a %b %d %Y" "C")
