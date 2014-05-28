36737831b642824fd25fd6415af82e84fd8649e2
Ding-Yi Chen <dingyichen@gmail.com>
2014-05-28 15:44:53 +1000

    CMAKE_FEDORA_MODULE_DIR: cmake-fedora modules dir

--- Modules/ManageEnvironment.cmake
+++ Modules/ManageEnvironment.cmake
@@ -67,108 +67,117 @@
 #    + PRJ_DOC_DIR: DocuFILEPATH = File chooser dialog.
 #      Default: ${DOC_DIR}/${PROJECT_NAME}-${PRJ_VER}
 
-IF(NOT DEFINED _MANAGE_ENVIRONMENT_CMAKE_)
-    SET(_MANAGE_ENVIRONMENT_CMAKE_ "DEFINED")
-    SET(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS ON)
+IF(DEFINED _MANAGE_ENVIRONMENT_CMAKE_)
+    RETURN()
+ENDIF(DEFINED _MANAGE_ENVIRONMENT_CMAKE_)
+SET(_MANAGE_ENVIRONMENT_CMAKE_ "DEFINED")
+SET(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS ON)
 
-    MACRO(SET_COMPILE_ENV var default_value)
-	SET(_stage "")
-	SET(_env "${var}")
-	SET(_setOpts "")
-	SET(_force 0)
-	FOREACH(_arg ${ARGN})
-	    IF(_arg STREQUAL "ENV_NAME")
-		SET(_stage "ENV_NAME")
-	    ELSEIF(_arg STREQUAL "CACHE")
-		SET(_stage "_CACHE")
-	    ELSE(_arg STREQUAL "ENV_NAME")
-		IF(_stage STREQUAL "ENV_NAME")
-		    SET(_env "${_arg}")
-		ELSEIF(_stage STREQUAL "_CACHE")
-		    LIST(APPEND _setOpts "${_arg}")
-		    IF(_arg STREQUAL "FORCE")
-			SET(_force 1)
-		    ENDIF(_arg STREQUAL "FORCE")
-		ENDIF(_stage STREQUAL "ENV_NAME")
-	    ENDIF(_arg STREQUAL "ENV_NAME")
-	ENDFOREACH(_arg ${ARGN})
+MACRO(SET_COMPILE_ENV var default_value)
+    SET(_stage "")
+    SET(_env "${var}")
+    SET(_setOpts "")
+    SET(_force 0)
+    FOREACH(_arg ${ARGN})
+	IF(_arg STREQUAL "ENV_NAME")
+	    SET(_stage "ENV_NAME")
+	ELSEIF(_arg STREQUAL "CACHE")
+	    SET(_stage "_CACHE")
+	ELSE(_arg STREQUAL "ENV_NAME")
+	    IF(_stage STREQUAL "ENV_NAME")
+		SET(_env "${_arg}")
+	    ELSEIF(_stage STREQUAL "_CACHE")
+		LIST(APPEND _setOpts "${_arg}")
+		IF(_arg STREQUAL "FORCE")
+		    SET(_force 1)
+		ENDIF(_arg STREQUAL "FORCE")
+	    ENDIF(_stage STREQUAL "ENV_NAME")
+	ENDIF(_arg STREQUAL "ENV_NAME")
+    ENDFOREACH(_arg ${ARGN})
 
-	IF(NOT "${_setOpts}" STREQUAL "")
-	    LIST(INSERT _setOpts 0 "CACHE")
-	ENDIF(NOT "${_setOpts}" STREQUAL "")
+    IF(NOT "${_setOpts}" STREQUAL "")
+	LIST(INSERT _setOpts 0 "CACHE")
+    ENDIF(NOT "${_setOpts}" STREQUAL "")
 
-	# Set the variable
-	IF(_force)
-	    IF(NOT "$ENV{${_env}}" STREQUAL "")
-		SET(${var} "$ENV{${_env}}" ${_setOpts})
-	    ELSE(NOT "$ENV{${_env}}" STREQUAL "")
-		SET(${var} "${default_value}" ${_setOpts})
-	    ENDIF(NOT "$ENV{${_env}}" STREQUAL "")
-	ELSE(_force)
-	    IF(NOT "${${var}}" STREQUAL "")
-		SET(${var} "${${var}}" ${_setOpts})
-	    ELSEIF(NOT "$ENV{${_env}}" STREQUAL "")
-		SET(${var} "$ENV{${_env}}" ${_setOpts})
-	    ELSE(NOT "${${var}}" STREQUAL "")
-		# Default value
-		SET(${var} "${default_value}" ${_setOpts})
-	    ENDIF(NOT "${${var}}" STREQUAL "")
-	ENDIF(_force)
+    # Set the variable
+    IF(_force)
+	IF(NOT "$ENV{${_env}}" STREQUAL "")
+	    SET(${var} "$ENV{${_env}}" ${_setOpts})
+	ELSE(NOT "$ENV{${_env}}" STREQUAL "")
+	    SET(${var} "${default_value}" ${_setOpts})
+	ENDIF(NOT "$ENV{${_env}}" STREQUAL "")
+    ELSE(_force)
+	IF(NOT "${${var}}" STREQUAL "")
+	    SET(${var} "${${var}}" ${_setOpts})
+	ELSEIF(NOT "$ENV{${_env}}" STREQUAL "")
+	    SET(${var} "$ENV{${_env}}" ${_setOpts})
+	ELSE(NOT "${${var}}" STREQUAL "")
+	    # Default value
+	    SET(${var} "${default_value}" ${_setOpts})
+	ENDIF(NOT "${${var}}" STREQUAL "")
+    ENDIF(_force)
 
-	# Enforce CMP0005 to new, yet pop after ADD_DEFINITION
-	CMAKE_POLICY(PUSH)
-	CMAKE_POLICY(SET CMP0005 NEW)
-	ADD_DEFINITIONS(-D${_env}=${${var}})
-	CMAKE_POLICY(POP)
-	M_MSG(${M_INFO2} "SET(${var} ${${var}})")
-    ENDMACRO(SET_COMPILE_ENV var default_value)
+    # Enforce CMP0005 to new, yet pop after ADD_DEFINITION
+    CMAKE_POLICY(PUSH)
+    CMAKE_POLICY(SET CMP0005 NEW)
+    ADD_DEFINITIONS(-D${_env}=${${var}})
+    CMAKE_POLICY(POP)
+    M_MSG(${M_INFO2} "SET(${var} ${${var}})")
+ENDMACRO(SET_COMPILE_ENV var default_value)
 
-    MACRO(MANAGE_CMAKE_POLICY policyName defaultValue)
-	IF(POLICY ${policyName})
-	    CMAKE_POLICY(GET "${policyName}" _cmake_policy_value)
-	    IF(_cmake_policy_value STREQUAL "")
-		# Policy not defined yet
-		CMAKE_POLICY(SET "${policyName}" "${defaultValue}")
-	    ENDIF(_cmake_policy_value STREQUAL "")
-	ENDIF(POLICY ${policyName})
-    ENDMACRO(MANAGE_CMAKE_POLICY policyName defaultValue)
+MACRO(MANAGE_CMAKE_POLICY policyName defaultValue)
+    IF(POLICY ${policyName})
+	CMAKE_POLICY(GET "${policyName}" _cmake_policy_value)
+	IF(_cmake_policy_value STREQUAL "")
+	    # Policy not defined yet
+	    CMAKE_POLICY(SET "${policyName}" "${defaultValue}")
+	ENDIF(_cmake_policy_value STREQUAL "")
+    ENDIF(POLICY ${policyName})
+ENDMACRO(MANAGE_CMAKE_POLICY policyName defaultValue)
 
-    # Include should be put after the cmake policy
-    INCLUDE(ManageMessage)
-    M_MSG(${M_INFO1} "CMAKE_HOST_SYSTEM=${CMAKE_HOST_SYSTEM}")
-    M_MSG(${M_INFO1} "CMAKE_SYSTEM=${CMAKE_SYSTEM}")
+# Include should be put after the cmake policy
+INCLUDE(ManageMessage)
+M_MSG(${M_INFO1} "CMAKE_HOST_SYSTEM=${CMAKE_HOST_SYSTEM}")
+M_MSG(${M_INFO1} "CMAKE_SYSTEM=${CMAKE_SYSTEM}")
 
-    ####################################################################
-    # CMake Variables
-    #
-    SET_COMPILE_ENV(BIN_DIR  "${CMAKE_INSTALL_PREFIX}/bin"
-	CACHE PATH "Binary dir")
-    SET_COMPILE_ENV(DATA_DIR "${CMAKE_INSTALL_PREFIX}/share"
-	CACHE PATH "Data dir")
-    SET_COMPILE_ENV(DOC_DIR  "${DATA_DIR}/doc"
-	CACHE PATH "Documentation dir")
-    SET_COMPILE_ENV(SYSCONF_DIR "/etc"
-	CACHE PATH "System configuration dir")
-    SET_COMPILE_ENV(LIBEXEC_DIR "${CMAKE_INSTALL_PREFIX}/libexec"
-	CACHE PATH "LIBEXEC dir")
+####################################################################
+# CMake Variables
+#
+SET_COMPILE_ENV(BIN_DIR  "${CMAKE_INSTALL_PREFIX}/bin"
+    CACHE PATH "Binary dir")
+SET_COMPILE_ENV(DATA_DIR "${CMAKE_INSTALL_PREFIX}/share"
+    CACHE PATH "Data dir")
+SET_COMPILE_ENV(DOC_DIR  "${DATA_DIR}/doc"
+    CACHE PATH "Documentation dir")
+SET_COMPILE_ENV(SYSCONF_DIR "/etc"
+    CACHE PATH "System configuration dir")
+SET_COMPILE_ENV(LIBEXEC_DIR "${CMAKE_INSTALL_PREFIX}/libexec"
+    CACHE PATH "LIBEXEC dir")
 
-    IF(CMAKE_SYSTEM_PROCESSOR MATCHES "64")
-	SET_COMPILE_ENV(IS_64 "64" CACHE STRING "IS_64")
-    ENDIF(CMAKE_SYSTEM_PROCESSOR MATCHES "64")
+IF(CMAKE_SYSTEM_PROCESSOR MATCHES "64")
+    SET_COMPILE_ENV(IS_64 "64" CACHE STRING "IS_64")
+ENDIF(CMAKE_SYSTEM_PROCESSOR MATCHES "64")
 
-    SET_COMPILE_ENV(LIB_DIR "${CMAKE_INSTALL_PREFIX}/lib${IS_64}"
-	CACHE PATH "Library dir")
+SET_COMPILE_ENV(LIB_DIR "${CMAKE_INSTALL_PREFIX}/lib${IS_64}"
+    CACHE PATH "Library dir")
 
-    SET_COMPILE_ENV(PROJECT_NAME "${PROJECT_NAME}")
-    SET_COMPILE_ENV(PRJ_DATA_DIR "${DATA_DIR}/${PROJECT_NAME}")
+SET_COMPILE_ENV(PROJECT_NAME "${PROJECT_NAME}")
+SET_COMPILE_ENV(PRJ_DATA_DIR "${DATA_DIR}/${PROJECT_NAME}")
 
-    # Directory to store cmake-fedora specific temporary files.
-    IF(NOT CMAKE_FEDORA_TMP_DIR)
-	SET(CMAKE_FEDORA_TMP_DIR "${CMAKE_BINARY_DIR}/NO_PACK")
-    ENDIF(NOT CMAKE_FEDORA_TMP_DIR)
+# Directory to store cmake-fedora specific temporary files.
+IF(NOT CMAKE_FEDORA_TMP_DIR)
+    SET(CMAKE_FEDORA_TMP_DIR "${CMAKE_BINARY_DIR}/NO_PACK")
+ENDIF(NOT CMAKE_FEDORA_TMP_DIR)
 
-    ADD_CUSTOM_COMMAND(OUTPUT ${CMAKE_FEDORA_TMP_DIR}
-	COMMAND cmake -E make_directory ${CMAKE_FEDORA_TMP_DIR}
-	COMMENT "Create CMAKE_FEDORA_TMP_DIR"
-	)
-ENDIF(NOT DEFINED _MANAGE_ENVIRONMENT_CMAKE_)
+# Current cmake-fedora module
+
+GET_FILENAME_COMPONENT(CMAKE_FEDORA_MODULE_DIR 
+    "${MANAGE_ENVIRONMENT_PATH}" PATH)
+SET(CMAKE_FEDORA_MODULE_DIR "${CMAKE_FEDORA_MODULE_DIR}"
+    CACHE PATH "cmake-fedora Modules path" FORCE)
+
+ADD_CUSTOM_COMMAND(OUTPUT ${CMAKE_FEDORA_TMP_DIR}
+    COMMAND cmake -E make_directory ${CMAKE_FEDORA_TMP_DIR}
+    COMMENT "Create CMAKE_FEDORA_TMP_DIR"
+    )
+
