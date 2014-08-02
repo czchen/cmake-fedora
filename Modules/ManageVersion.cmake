40e8b7ae0558d227616975b4dca6b55e53122586
Ding-Yi Chen <dingyichen@gmail.com>
2014-08-02 15:36:56 +1000

    PRJ_INFO_CMAKE_APPEND: swap parameters, as prj_info_cmake should be optional

--- Modules/ManageVersion.cmake
+++ Modules/ManageVersion.cmake
@@ -1,49 +1,56 @@
-# - Modules for manipulate versions and prj_info.cmake
+# - Manage Version and project information (prj_info.cmake)
 #
-# Includes:
-#   ManageVariable
-#   DateTimeFormat
+# Included Modules:
+#   - DateTimeFormat
+#   - ManageString
+#   - ManageVariable
 #
-# Included by:
-#   ManageArchive
+# Set cache for following variables:
+#   - PRJ_INFO_CMAKE_FILE: 
 #
 # Defines following functions:
-#   RELEASE_NOTES_READ_FILE([releaseFile])
+#   RELEASE_NOTES_READ_FILE([<release_file>])
 #   - Load release file information.
-#     Arguments:
-#     + releaseFile: (Optional) release file to be read.
-#       This file should contain following definition:
-#       - PRJ_VER: Release version.
-#       - SUMMARY: Summary of the release. Will be output as CHANGE_SUMMARY.
-#       - Section [Changes]:
-#         Changes of this release list below the section tag.
-#       Default:RELEASE-NOTES.txt
-#     This macro writes following files:
-#     + PRJ_INFO_CMAKE: ${CMAKE_FEDORA_TMP_DIR}/prj_info.cmake
-#       Project information files to be included by scripts.
+#     * Parameters:
+#       + release_file: (Optional) release file to be read.
+#         This file should contain following definition:
+#         - PRJ_VER: Release version.
+#         - SUMMARY: Summary of the release. Will be output as CHANGE_SUMMARY.
+#         - Section [Changes]:
+#           Changes of this release list below the section tag.
+#         Default:${CMAKE_SOURCE_DIR}/RELEASE-NOTES.txt
+#     * Values to cached:
+#       + PRJ_VER: Version.
+#       + CHANGE_SUMMARY: Summary of changes.
+#       + RELEASE_NOTES_FILE: The loaded release file.
+#     * Compile flags defined:
+#       + PRJ_VER: Project version.
 #
-#     This function defines following variables:
-#     + PRJ_VER: Release version.
-#     + CHANGE_SUMMARY: Summary of changes.
-#     + RELEASE_NOTES_FILE: The loaded release file.
-#     + PRJ_DOC_DIR: Documentation for the project.
-#       Default: ${DOC_DIR}/${PROJECT_NAME}-${PRJ_VER}
+#   PRJ_INFO_CMAKE_WRITE([<prj_info_file>])
+#   - Write the project infomation to prj_info.cmake/
+#     * Parameters:
+#       + prj_info_file: (Optional) File name to write project information.
+#         Default: ${CMAKE_FEDORA_TMP_DIR}/prj_info.cmake
+#     * Values to cached:
+#       + PRJ_INFO_CMAKE: Path to prj_info.cmake
+#         Default: ${CMAKE_FEDORA_TMP_DIR}/prj_info.cmake
 #
-#     This function defines following compile flags:
-#     + PRJ_VER
-#
-#   PRJ_INFO_CMAKE_APPEND(<prjInfoFile> <var>)
+#   PRJ_INFO_CMAKE_APPEND(<var> [<prj_info_file>])
 #   - Append  var to prj_info.cmake.
-#     Arguments:
-#     + prjInfoFile: Location of prj_info.cmake
-#     + var: Variable to be processed.
+#     * Parameters:
+#       + var: Variable to be append to prj_info.cmake.
+#       + prj_info_file: (Optional) File name to be appended to.
+#         Default: ${PRJ_INFO_CMAKE}, otherwise ${CMAKE_FEDORA_TMP_DIR}/prj_info.cmake.
+#     * Values to cached:
+#       + PRJ_INFO_CMAKE: Path to prj_info.cmake
+#         Default: ${CMAKE_FEDORA_TMP_DIR}/prj_info.cmake
 #
 # Defines following macros:
-#   PRJ_INFO_CMAKE_READ(<prjInfoFile>)
+#   PRJ_INFO_CMAKE_READ(<prj_info_file>)
 #   - Read prj_info.cmake and get the info of projects.
 #     This macro is meant to be run by cmake script.
 #     Arguments:
-#     + prjInfoFile: Location of prj_info.cmake
+#     + prj_info_file: Location of prj_info.cmake
 #
 
 IF(DEFINED _MANAGE_VERSION_CMAKE_)
@@ -60,34 +67,45 @@ SET(PRJ_INFO_VARIABLE_LIST
     BUILD_ARCH RPM_SPEC_URL RPM_SPEC_SOURCES
     )
 
-FUNCTION(PRJ_INFO_CMAKE_APPEND prjInfoFile var)
+FUNCTION(PRJ_INFO_CMAKE_WHICH var prj_info_file)
+    IF("${prj_info_file}" STREQUAL "")
+	IF("${PRJ_INFO_CMAKE}" STREQUAL "")
+	    SET(PRJ_INFO_CMAKE "${CMAKE_FEDORA_TMP_DIR}/prj_info.cmake" CACHE INTERNAL "prj_info.cmake")
+	ENDIF()
+	SET(${var} "${PRJ_INFO_CMAKE}" PARENT_SCOPE)
+    ELSE()
+	SET(${var} "${prj_info_file}" PARENT_SCOPE)
+    ENDIF()
+    
+ENDFUNCTION(PRJ_INFO_CMAKE_WHICH)
+
+FUNCTION(PRJ_INFO_CMAKE_APPEND var prj_info_file)
+    PRJ_INFO_CMAKE_WHICH(outFile ${prj_info_file})
     IF(NOT "${${var}}" STREQUAL "")
 	STRING_ESCAPE_BACKSLASH(_str "${${var}}")
 	STRING_ESCAPE_DOLLAR(_str "${_str}")
 	STRING_ESCAPE_QUOTE(_str "${_str}")
-	FILE(APPEND ${prjInfoFile} "SET(${var} \"${_str}\")\n")
+	FILE(APPEND ${outFile} "SET(${var} \"${_str}\")\n")
     ENDIF(NOT "${${var}}" STREQUAL "")
 ENDFUNCTION(PRJ_INFO_CMAKE_APPEND)
 
-MACRO(PRJ_INFO_CMAKE_READ prjInfoFile)
-    IF("${prjInfoFile}" STREQUAL "")
+MACRO(PRJ_INFO_CMAKE_READ prj_info_file)
+    IF("${prj_info_file}" STREQUAL "")
 	M_MSG(${M_EROR} "Requires prj_info.cmake")
     ENDIF()
-    INCLUDE(${prjInfoFile} RESULT_VARIABLE prjInfoPath)
+    INCLUDE(${prj_info_file} RESULT_VARIABLE prjInfoPath)
     IF("${prjInfoPath}" STREQUAL "NOTFOUND")
-	M_MSG(${M_ERROR} "Failed to read ${prjInfoFile}")
+	M_MSG(${M_ERROR} "Failed to read ${prj_info_file}")
     ENDIF()
 ENDMACRO(PRJ_INFO_CMAKE_READ)
 
-# Write Project info to prj_info.cmake
-# So scripts like ManageChangeLogScript andManageRPMScript 
-# can retrieve project information
-FUNCTION(PRJ_INFO_CMAKE_WRITE prjInfoFile)
-    FILE(REMOVE ${prjInfoFile})
+FUNCTION(PRJ_INFO_CMAKE_WRITE prj_info_file)
+    PRJ_INFO_CMAKE_WHICH(outFile ${prj_info_file})
+    FILE(REMOVE "${outFile}")
     FOREACH(_v ${PRJ_INFO_VARIABLE_LIST})
-	PRJ_INFO_CMAKE_APPEND("${prjInfoFile}" ${_v})
+	PRJ_INFO_CMAKE_APPEND(${_v} "${outFile}")
     ENDFOREACH(_v)
-ENDFUNCTION(PRJ_INFO_CMAKE_WRITE prjInfoFile)
+ENDFUNCTION(PRJ_INFO_CMAKE_WRITE prj_info_file)
 
 FUNCTION(RELEASE_NOTES_READ_FILE)
     INCLUDE(ManageString)
@@ -129,9 +147,9 @@ FUNCTION(RELEASE_NOTES_READ_FILE)
 	ENDIF(_changeItemSection)
     ENDFOREACH(_line ${_release_lines})
 
-    SET(PRJ_INFO_CMAKE "${CMAKE_FEDORA_TMP_DIR}/prj_info.cmake"
+    SET(PRJ_INFO_CMAKE_FILE "${CMAKE_FEDORA_TMP_DIR}/prj_info.cmake"
 	CACHE FILEPATH "prj_info.cmake")
 
-    PRJ_INFO_CMAKE_WRITE(${PRJ_INFO_CMAKE})
+    PRJ_INFO_CMAKE_WRITE(${PRJ_INFO_CMAKE_FILE})
 ENDFUNCTION(RELEASE_NOTES_READ_FILE)
 
